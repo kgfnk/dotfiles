58be64610331c70cef3c3a59f20c7f1f8a1c43ef
kgfnk <kgfnk@users.noreply.github.com>
2014-05-17 01:58:49 +0900

    リポジトリ移動

---
+++ .vim/ftplugin/ruby.vim
@@ -0,0 +1,8 @@
+if exists("b:did_ftplugin")
+    finish
+endif
+let b:did_ftplugin=1
+setlocal tabstop=2
+setlocal autoindent
+setlocal expandtab
+setlocal shiftwidth=2
---
+++ .vim/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#実行方法
+#chmod 755 setup.sh
+#./setup.sh
+ln -s ~/Dropbox/.vim/ftplugin/ ~/.vim/ftplugin
+ln -s ~/Dropbox/.vim/snippets/ ~/.vim/snippets
+ln -s ~/Dropbox/.vim/template/ ~/.vim/template
