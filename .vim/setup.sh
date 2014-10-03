1aab584142d28be5340695714979268528dbea24
kgfnk <kgfnk@users.noreply.github.com>
2014-10-03 11:53:32 +0900

    ftpluginをafterの下へ移動

--- .vim/setup.sh
+++ .vim/setup.sh
@@ -3,6 +3,6 @@
 #chmod 755 setup.sh
 #./setup.sh
 DIR=$(cd $(dirname $0); pwd)
-ln -s ${DIR}/ftplugin/ ~/.vim/ftplugin
+ln -s ${DIR}/after/ ~/.vim/after
 ln -s ${DIR}/snippets/ ~/.vim/snippets
 ln -s ${DIR}/template/ ~/.vim/template
